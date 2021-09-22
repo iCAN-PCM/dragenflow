6f8376f2a388e8f1d20fba9a6d12cbed8b59d419
Biranjan <biranjan.raut@gmail.com>
2021-09-22 15:41:10 +0300

    rna pipeline

---
+++ src/dragen_rna_pipeline.py
@@ -0,0 +1,9 @@
+from .utility.flow import Flow
+
+
+class ConstructRnaPipeline(Flow):
+    def __init__(self) -> None:
+        return
+
+    def constructor(self, data: dict):
+        return super().constructor(data)
